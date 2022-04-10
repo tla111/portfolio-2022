4b7c43a95834b388fd765d2bac39043c1fb8097d
Tim La <tlamarketing111@gmail.com>
2022-04-10 10:49:55 -0400

    fixed typo in index for exporting components

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { Header, About, Work, Skills, Testimonial, Footer } from './container';
+import { Navbar } from './components';
 
 const App = () => {
     return (
         <div className="app">
+            <Navbar />
             <Header />
             <About />
             <Work />
