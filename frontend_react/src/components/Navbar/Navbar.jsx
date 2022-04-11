ec248fd516bc9e0b86966081c47a1318c0fc072e
Tim La <tlamarketing111@gmail.com>
2022-04-11 09:43:42 -0400

    styled navbar and navbar logo

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import { images } from '../../constants';
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
                 <img src={images.logo} alt="logo" />
             </div>
-            <ul>
+            <ul className="app__navbar-links">
                 {["home", "about", "work", "skills", "contact"].map((item) => (
-                    <li key={`link-${item}`}>
+                    <li className="app__flex p-text" key={`link-${item}`}>
                         <div />
                         <a href={`#${item}`}>{item}</a>
                     </li>
