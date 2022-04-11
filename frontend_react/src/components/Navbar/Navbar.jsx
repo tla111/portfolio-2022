5c0f2b04c30f8d33eec8fead537466a3980bc06b
Tim La <tlamarketing111@gmail.com>
2022-04-11 10:03:39 -0400

    added ul to mobile view

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -30,11 +30,14 @@ const Navbar = () => {
                         transition={{ duration: 0.85, ease: "easeOut" }}
                     >
                         <HiX onClick={() => setToggle(false)} />
-                        {["home", "about", "work", "skills", "contact"].map((item) => (
-                            <li key={item}>
-                                <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                            </li>
-                        ))}
+                        <ul className="app__navbar-links">
+                            {["home", "about", "work", "skills", "contact"].map((item) => (
+                                <li className="app__flex p-text" key={`link-${item}`}>
+                                    <div />
+                                    <a href={`#${item}`}>{item}</a>
+                                </li>
+                            ))}
+                        </ul>
                     </motion.div>
                 )}
             </div>
