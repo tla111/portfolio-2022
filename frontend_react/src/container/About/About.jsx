3489a4576e712fbbee1aa946d88652949e6c2b8d
Tim La <tlamarketing111@gmail.com>
2022-04-12 11:01:48 -0400

    added mock data in about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import './About.scss';
 
+const abouts = [
+    { title: "Web Development", description: "I am a good web developer", imgUrl: "" },
+    { title: "Web Design", description: "I am a good web developer", imgUrl: "" },
+    { title: "UI/UX", description: "I am a good web developer", imgUrl: "" },
+    { title: "Web Animations", description: "I am a good web developer", imgUrl: "" }
+]
+
 const About = () => {
     return (
         <>
@@ -12,6 +19,22 @@ const About = () => {
                 means
                 <span>Good Business</span>
             </h2>
+
+            <div className="app__profiles">
+                {abouts.map((about, index) => (
+                    <motion.div
+                        whileInView={{ opacity: 1 }}
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ duration: 0.5, type: "tween" }}
+                        className="app__profile-item"
+                        key={about.title + index}
+                    >
+                        <img src={about.imgUrl} alt={about.title} />
+                        <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+                        <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+                    </motion.div>
+                ))}
+            </div>
         </>
     )
 }
