9f0fe5243ac7e819c7c353df834bb958b46bc118
Tim La <tlamarketing111@gmail.com>
2022-04-12 11:09:45 -0400

    changed titles

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -5,9 +5,9 @@ import './About.scss';
 
 const abouts = [
     { title: "Web Development", description: "I am a good web developer", imgUrl: images.about01 },
-    { title: "Web Design", description: "I am a good web developer", imgUrl: images.about02 },
-    { title: "UI/UX", description: "I am a good web developer", imgUrl: images.about03 },
-    { title: "Web Animations", description: "I am a good web developer", imgUrl: images.about04 }
+    { title: "Frontend Development", description: "I am a good web developer", imgUrl: images.about02 },
+    { title: "Backend Development", description: "I am a good web developer", imgUrl: images.about03 },
+    { title: "MERN Stack", description: "I am a good web developer", imgUrl: images.about04 }
 ]
 
 const About = () => {
