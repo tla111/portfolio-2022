6378af5b3b891581477a7e406618b777d7e8711c
Tim La <tlamarketing111@gmail.com>
2022-04-12 11:15:30 -0400

    added information to connect sanity to frontend

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,9 +2,9 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: "",
-    dataset: "",
-    apiVersion: "",
+    projectId: "y8g5cu80",
+    dataset: "production",
+    apiVersion: "2022-02-01",
     useCdn: true,
-    token: "",
+    token: "skDfdBS3BZZyHdAClDPEmifMS47CHioIY18zDlBx3tETq3y5diq9HYDgZKMWSKcx0Id7ejMEYvRuvCqpLRkSmUfBjiEk3FCMOQvusUbAOkaA4IVQENXL5XluZaamanCQ3iJdVDlgMwdSWNauzi4wuB7tlKQOU0WhFa2C72Eql5Wyj4AJ41pS",
 });
