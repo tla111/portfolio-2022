f1829f89778eabeb93265539f420cc0fa8394240
Tim La <tlamarketing111@gmail.com>
2022-04-13 11:10:34 -0400

    created logic for work component

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -6,11 +6,27 @@ import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+    const [activeFilter, setActiveFilter] = useState("All");
+
+    const handleWorkFilter = (item) => {
+
+    }
+
     return (
         <>
             <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
-
+            <div className="app__work-filter">
+                {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map((item, index) => (
+                    <div
+                        key={index}
+                        onClick={() => handleWorkFilter(item)}
+                        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? "item-active" : ""}`}
+                    >
+                        {item}
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
