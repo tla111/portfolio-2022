cd30095c8045b18b949c9b7d52761e4648731d75
Tim La <tlamarketing111@gmail.com>
2022-04-13 11:32:07 -0400

    implemented anchor tag

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -50,6 +50,23 @@ const Work = () => {
                     <div className="app__work-item app__flex" key={index}>
                         <div className="app__work-img app__flex">
                             <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+                            <motion.div
+                                whileHover={{ opacity: [0, 1] }}
+                                transition={{ duration: 0.25, ease: "easeInOut", staggerChildren: 0.5 }}
+                                className="app__work-hover app__flex"
+                            >
+                                <a href={work.projectLink} target="_blank" rel="noreffer">
+                                    <motion.div
+                                        whileInView={{ scale: [0, 1] }}
+                                        whileHover={{ scale: [1, 0.9] }}
+                                        transition={{ duration: 0.25 }}
+                                        className="app__flex"
+                                    >
+                                        <AiFillEye />
+                                    </motion.div>
+                                </a>
+                            </motion.div>
                         </div>
                     </div>
                 ))}
