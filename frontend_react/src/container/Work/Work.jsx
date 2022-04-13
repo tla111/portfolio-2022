3e083e86982c823234d79795c414052a7e13adaf
Tim La <tlamarketing111@gmail.com>
2022-04-13 11:04:31 -0400

    imported components & added header in work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
     return (
-        <div>Work</div>
+        <>
+            <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+
+
+        </>
     )
 }
 
