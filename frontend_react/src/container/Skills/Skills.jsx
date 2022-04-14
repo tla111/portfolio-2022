c2d1f7e817397032920aca9e9ceb23cfd8e37fd7
Tim La <tlamarketing111@gmail.com>
2022-04-14 12:38:08 -0400

    able to render year and work experience using nested loop

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -54,31 +54,33 @@ const Skills = () => {
                             <div className="app__skills-exp-year">
                                 <p className="bold-text">{experience.year}</p>
                             </div>
+                            <motion.div className="app__skills-exp-works">
+                                {experience.works.map((work) => (
+                                    <>
+                                        <motion.div
+                                            whileInView={{ opacity: [0, 1] }}
+                                            transition={{ duration: 0.5 }}
+                                            className="app__skills-exp-work"
+                                            data-tip
+                                            data-for={work.name}
+                                            key={work.name}
+                                        >
+                                            <h4 className="bold-text">{work.name}</h4>
+                                            <p className="p-text">{work.company}</p>
+                                        </motion.div>
+                                        <ReactTooltip
+                                            id={work.name}
+                                            effect="solid"
+                                            arrowColor="#fff"
+                                            className="skills-tooltip"
+                                        >
+                                            {work.desc}
+                                        </ReactTooltip>
+                                    </>
+                                ))}
+                            </motion.div>
                         </motion.div>
                     ))}
-                    {/* {experience?.map((work) => (
-                        <>
-                            <motion.div
-                                whileInView={{ opacity: [0, 1] }}
-                                transition={{ duration: 0.5 }}
-                                className="app__skills-exp-work"
-                                data-tip
-                                data-for={work.name}
-                                key={work.name}
-                            >
-                                <h4 className="bold-text">{work.name}</h4>
-                                <p className="p-text">{work.company}</p>
-                            </motion.div>
-                            <ReactTooltip
-                                id={work.name}
-                                effect="solid"
-                                arrowColor="#fff"
-                                className="skills-tooltip"
-                            >
-                                {work.desc}
-                            </ReactTooltip>
-                        </>
-                    ))} */}
                 </motion.div>
             </div>
         </>
