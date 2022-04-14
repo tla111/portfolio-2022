98fa477bd37cc95bf86eeb576495902936d5f8a3
Tim La <tlamarketing111@gmail.com>
2022-04-14 12:57:28 -0400

    created logic for motionWrap function

--- frontend_react/src/wrapper/MotionWrap.js
+++ frontend_react/src/wrapper/MotionWrap.js
@@ -3,7 +3,13 @@ import { motion } from 'framer-motion';
 
 const MotionWrap = ({ Component, classNames }) => function HOC() {
     return (
-        <motion.div></motion.div>
+        <motion.div
+            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+            transition={{ duration: 0.5 }}
+            className={`${classNames} app__flex`}
+        >
+            <Component />
+        </motion.div>
     )
 }
 
