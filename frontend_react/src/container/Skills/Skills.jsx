d0cb2b4d788fc5a22ab4b560fea73f3fa7bfeaee
Tim La <tlamarketing111@gmail.com>
2022-04-14 11:50:56 -0400

    created logic to retrieve skills & experiences

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -6,9 +6,29 @@ import { urlFor, client } from '../../client';
 import './Skills.scss';
 
 const Skills = () => {
+    const [experience, setExperience] = useState([]);
+    const [skills, setSkills] = useState([]);
+
+    useEffect(() => {
+        const query = '*[_type == "experiences"]';
+        const skillsQuery = '*[_type == "skills"]';
+
+        client.fetch(query).then((data) => {
+            setExperience(data);
+        })
+        client.fetch(skillsQuery).then((data) => {
+            setSkills(data);
+        })
+    }, [])
     return (
         <>
             <h2 className="head-text">Skills & Experience</h2>
+
+            <div className="app__skills-container">
+                <motion.div className="app__skills-list">
+                    {/* {skills.map} */}
+                </motion.div>
+            </div>
         </>
     )
 }
