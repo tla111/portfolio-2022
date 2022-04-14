e7b591724ff14dd3d6fabf627fffb9df9ac1abfd
Tim La <tlamarketing111@gmail.com>
2022-04-14 11:53:56 -0400

    created logic to display skill

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -26,7 +26,19 @@ const Skills = () => {
 
             <div className="app__skills-container">
                 <motion.div className="app__skills-list">
-                    {/* {skills.map} */}
+                    {skills.map((skill) => (
+                        <motion.div
+                            whileInView={{ opacity: [0, 1] }}
+                            transition={{ duration: 0.5 }}
+                            className="app__skills-item app__flex"
+                            key={skill.name}
+                        >
+                            <div className="app__flex" style={{ backgroundColor: skill.bgColor }}>
+                                <img src={urlFor(skill.icon)} alt={skill.name} />
+                            </div>
+                            <p className="p-text">{skill.name}</p>
+                        </motion.div>
+                    ))}
                 </motion.div>
             </div>
         </>
