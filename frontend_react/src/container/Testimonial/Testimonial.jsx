22e7aa769b47f928d45782c1e0ea1d41ed827714
Tim La <tlamarketing111@gmail.com>
2022-04-15 11:33:08 -0400

    imported necessities & created states in testimonials component

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
+    const [brands, setBrands] = useState([]);
+    const [testimonials, setTestimonials] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
     return (
-        <div>Testimonial</div>
+        <div>
+
+        </div>
     )
 }
 
