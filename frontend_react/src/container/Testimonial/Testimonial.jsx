bcccc273e9e727e69db7bc27b7bc52bfbe334f7f
Tim La <tlamarketing111@gmail.com>
2022-04-15 11:34:51 -0400

    created logic for useEffect in testimonials component

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -11,6 +11,21 @@ const Testimonial = () => {
     const [brands, setBrands] = useState([]);
     const [testimonials, setTestimonials] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const query = '*[_type == "testimonials"]';
+        const brandsQuery = '*[_type == "brands"]';
+
+        client.fetch(query).then((data) => {
+            // console.log(data);
+            setTestimonials(data);
+        })
+        client.fetch(brandsQuery).then((data) => {
+            // console.log(data);
+
+            setBrands(data);
+        })
+    }, [])
     return (
         <div>
 
