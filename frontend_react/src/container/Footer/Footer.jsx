be42671b004cf0923458a8dd6bf3d11081ad5785
Tim La <tlamarketing111@gmail.com>
2022-04-16 11:43:24 -0400

    created contact form

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -20,6 +20,25 @@ const Footer = () => {
                     <a href="tel: +1 (123) 456-789" className="p-text">+1 (123) 456-789</a>
                 </div>
             </div>
+
+            <div className="app__footer-form app__flex">
+                <div className="app__flex">
+                    <input className="p-text" type="text" placeHolder="Your Name" name="name" value={name} onChange={handleChangeInput} />
+                </div>
+                <div className="app__flex">
+                    <input className="p-text" type="email" placeHolder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+                </div>
+                <div >
+                    <textarea
+                        className="p-text"
+                        placeHolder="Your Message"
+                        value={message}
+                        name={message}
+                        onChange={handleChangeInput}
+                    />
+                </div>
+                <button type="button" className="p-text" onClick={handleSubmit}>Send Message</button>
+            </div>
         </>
     )
 }
