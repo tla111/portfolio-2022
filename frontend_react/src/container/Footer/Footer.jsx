4b06ccba5c0ff2b1722858b8bbcff4b1620159c0
Tim La <tlamarketing111@gmail.com>
2022-04-16 11:38:30 -0400

    created contact information

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -10,11 +10,16 @@ const Footer = () => {
         <>
             <h2 className="head-text">Take a coffee & chat with me</h2>
 
-            {/* <div className="app__footer-cards">
-                <div>
-
+            <div className="app__footer-cards">
+                <div className="app__footer-cards">
+                    <img src={images.email} alt="email" />
+                    <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
                 </div>
-            </div> */}
+                {/* <div className="app__footer-cards">
+                    <img src={images.email} alt="email" />
+                    <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+                </div> */}
+            </div>
         </>
     )
 }
