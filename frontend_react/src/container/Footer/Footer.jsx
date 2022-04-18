ed381665ef40eb9432a5301854c64ae40743b0db
Tim La <tlamarketing111@gmail.com>
2022-04-18 11:53:37 -0400

    fixed typo

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -38,11 +38,11 @@ const Footer = () => {
             <h2 className="head-text">Take a coffee & chat with me</h2>
 
             <div className="app__footer-cards">
-                <div className="app__footer-cards">
+                <div className="app__footer-card">
                     <img src={images.email} alt="email" />
                     <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
                 </div>
-                <div className="app__footer-cards">
+                <div className="app__footer-card">
                     <img src={images.mobile} alt="mobile" />
                     <a href="tel: +1 (123) 456-789" className="p-text">+1 (123) 456-789</a>
                 </div>
