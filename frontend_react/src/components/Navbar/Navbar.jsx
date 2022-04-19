56b92b7fdf8ddb398afdebd609733b2d36f48fbe
Tim La <tlamarketing111@gmail.com>
2022-04-19 12:52:33 -0400

    removed logo from navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
 
     return (
         <nav className="app__navbar">
-            <div className="app__navbar-logo">
+            {/* <div className="app__navbar-logo">
                 <img src={images.logo} alt="logo" />
-            </div>
+            </div> */}
             <ul className="app__navbar-links">
                 {["home", "about", "work", "skills", "contact"].map((item) => (
                     <li className="app__flex p-text" key={`link-${item}`}>
