87c1a91a6b0e0bd5ff1b28bd3fbc3853223b3afa
Tim La <tlamarketing111@gmail.com>
2022-04-20 20:06:10 -0400

    uncomment first section in header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -20,7 +20,7 @@ import './Header.scss';
 const Header = () => {
     return (
         <div className="app__header app__flex">
-            {/* <motion.div
+            <motion.div
                 whileInView={{ x: [-100, 0], opacity: [0, 1] }}
                 transition={{ duration: 0.5 }}
                 className="app__header-info"
@@ -38,7 +38,7 @@ const Header = () => {
                         <p className="p-text">Freelancer</p>
                     </div>
                 </div>
-            </motion.div> */}
+            </motion.div>
 
             {/* <motion.div
                 whileInView={{ opacity: [0, 1] }}
