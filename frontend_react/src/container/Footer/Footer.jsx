876e612c26b497f1e57d06ac3e7c32eebfa5a9a2
Tim La <tlamarketing111@gmail.com>
2022-04-21 11:40:38 -0400

    changed email

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
             <div className="app__footer-cards">
                 <div className="app__footer-card">
                     <img src={images.email} alt="email" />
-                    <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+                    <a href="mailto:hello@micael.com" className="p-text">tlamarketing111@gmail.com</a>
                 </div>
                 <div className="app__footer-card">
                     <img src={images.mobile} alt="mobile" />
