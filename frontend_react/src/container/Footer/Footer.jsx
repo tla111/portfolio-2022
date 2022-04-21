7bcfe1539748857b04780a1affec2343651a5869
Tim La <tlamarketing111@gmail.com>
2022-04-21 11:55:24 -0400

    changed footer head text

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
     }
     return (
         <>
-            <h2 className="head-text">Take a coffee & chat with me</h2>
+            <h2 className="head-text">Looking Forward to Connect with You</h2>
 
             <div className="app__footer-cards">
                 <div className="app__footer-card">
