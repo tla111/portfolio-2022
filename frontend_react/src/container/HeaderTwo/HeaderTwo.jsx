d12907c04913a053ffd4347837795bca4deb9dbc
Tim La <tlamarketing111@gmail.com>
2022-04-22 12:32:20 -0400

    added motion to header

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-
+import { motion } from 'framer-motion';
 import { AppWrap } from '../../wrapper';
-
 import './HeaderTwo.scss';
 
 const HeaderTwo = () => {
     return (
         <div className="app__header">
-            <div className="header_box first_header_box">
+            <motion.div
+                whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="header_box first_header_box"
+            >
                 <h1>Tim La, Front-End Web Developer</h1>
                 <p>Lorem ipsum dolor sit amet,
                     consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam
                 </p>
-            </div>
-            <div className="header_box second_header_box">
+            </motion.div>
+            <motion.div
+                whileInView={{ x: [100, 0], opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="header_box second_header_box"
+            >
                 <div className="header_boxTwo_image_container"></div>
-            </div>
+            </motion.div>
         </div>
     )
 }
