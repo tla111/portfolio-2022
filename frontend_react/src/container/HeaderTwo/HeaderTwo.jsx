6fb86a6eb69b41dec7e77650651c153d59ccf578
Tim La <tlamarketing111@gmail.com>
2022-04-22 12:45:25 -0400

    added box shadow to header image

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
 import './HeaderTwo.scss';
 
 const HeaderTwo = () => {
@@ -11,7 +12,7 @@ const HeaderTwo = () => {
                 transition={{ duration: 0.5 }}
                 className="header_box first_header_box"
             >
-                <h1>Tim La, Front-End Web Developer</h1>
+                <h1>Tim La | Front-End Web Developer</h1>
                 <p>Lorem ipsum dolor sit amet,
                     consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam
@@ -19,10 +20,11 @@ const HeaderTwo = () => {
             </motion.div>
             <motion.div
                 whileInView={{ x: [100, 0], opacity: [0, 1] }}
+                whileHover={{ scale: 1.1 }}
                 transition={{ duration: 0.5 }}
                 className="header_box second_header_box"
             >
-                <div className="header_boxTwo_image_container"></div>
+                <img src={images.about01} alt="header-img" />
             </motion.div>
         </div>
     )
