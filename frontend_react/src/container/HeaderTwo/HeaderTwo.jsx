ec8154eddf2660ad88d02aece732480b00487b33
Tim La <tlamarketing111@gmail.com>
2022-04-22 12:26:22 -0400

    added image to header, working on first header box

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -7,8 +7,14 @@ import './HeaderTwo.scss';
 const HeaderTwo = () => {
     return (
         <div className="app__header">
-            <div className="header_box"></div>
-            <div className="header_box"></div>
+            <div className="header_box first_header_box">
+                <h1>Welcome to My Website</h1>
+                <h2>Welcome to My Website</h2>
+                <h3>Welcome to My Website</h3>
+            </div>
+            <div className="header_box second_header_box">
+                <div className="header_boxTwo_image_container"></div>
+            </div>
         </div>
     )
 }
