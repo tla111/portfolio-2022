8458fcd7199e7bc7b259f928ad5e80c6c4318132
Tim La <tlamarketing111@gmail.com>
2022-04-30 21:44:28 -0400

    hid header text

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -13,9 +13,9 @@ const HeaderTwo = () => {
                 className="header_box first_header_box"
             >
                 <h1>Tim La | Front-End Web Developer</h1>
-                <h3>
+                {/* <h3>
                     Seeking to build my curiosity and expertise in <br /><span style={{ color: "#00FFFF" }}>React, JavaScript, & Solidity</span>
-                </h3>
+                </h3> */}
             </motion.div>
             <motion.div
                 whileInView={{ x: [100, 0], opacity: [0, 1] }}
