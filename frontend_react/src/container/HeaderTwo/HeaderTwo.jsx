faf5c70949e55339dcacadf49d1d2f9f1e57030e
Tim La <tlamarketing111@gmail.com>
2022-05-22 13:13:55 -0400

    lowercase word

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -12,9 +12,9 @@ const HeaderTwo = () => {
                 transition={{ duration: 0.75 }}
                 className="header_box first_header_box"
             >
-                <h1>Tim La| Full Stack Web Developer 2022</h1>
+                <h1>Tim La | Full Stack Web Developer 2022</h1>
                 <h3>
-                    SEEKING to invigorate my curiosity & escalate my expertise in <br /><span style={{ color: "#00FFFF" }}>React, JavaScript, Sanity, Node, & Solidity!!!</span>
+                    Seeking to invigorate my curiosity & escalate my expertise in <br /><span style={{ color: "#00FFFF" }}>React, JavaScript, Sanity, Node, & Solidity!!!</span>
                 </h3>
             </motion.div>
             <motion.div
