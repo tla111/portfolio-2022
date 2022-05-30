484abe1ae2a4000031b45a1875bb06bbe9febb4f
Tim La <tlamarketing111@gmail.com>
2022-05-29 20:43:40 -0400

    removed extra last name

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -12,7 +12,7 @@ const HeaderTwo = () => {
                 transition={{ duration: 0.75 }}
                 className="header_box first_header_box"
             >
-                <h1>Tim LaLa | Full Stack Web DeveloperRahahah</h1>
+                <h1>Tim La | Full Stack Web DeveloperRahahah</h1>
                 <h3>
                     Seeking to invigorate my curiosity & escalate my expertise in <br /><span style={{ color: "#00FFFF" }}>RR3React, JavaScript, Sanity, Node, & Solidity...</span>
                 </h3>
