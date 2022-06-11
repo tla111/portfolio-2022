b721252036452c15057724e8cac979cf1fe79c32
Tim La <tlamarketing111@gmail.com>
2022-06-10 20:32:27 -0400

    added h2

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -17,6 +17,7 @@ const HeaderTwo = () => {
                     Seeking to invigorate my curiosity & escalate my expertise in <br /><span style={{ color: "#00FFFF" }}>React, JavaScript, Sanity, Node, & Solidity</span>
                 </h3>
                 <h1>Hi</h1>
+                <h2>Hi</h2>
             </motion.div>
             <motion.div
                 whileInView={{ x: [100, 0], opacity: [0, 1] }}
