5d8065e6306108aac1e9462a007f59306634f374
Tim La <tlamarketing111@gmail.com>
2022-10-07 20:43:55 -0400

    project looks good

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -16,6 +16,7 @@ const HeaderTwo = () => {
                 <h3>
                     Seeking to invigorate my curiosity & escalate my expertise in <br /><span style={{ color: "#00FFFF" }}>React, JavaScript, Sanity, Node, & Solidity...</span>
                 </h3>
+                <h3>Everything looks good</h3>
             </motion.div>
             <motion.div
                 whileInView={{ x: [100, 0], opacity: [0, 1] }}
