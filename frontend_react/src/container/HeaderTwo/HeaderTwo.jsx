d37fe968627fe41fd43a2ce0bc3d6199a7a80276
Tim La <tlamarketing111@gmail.com>
2023-03-26 06:20:46 -0400

    computer model displaying properly

--- frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
+++ frontend_react/src/container/HeaderTwo/HeaderTwo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
+import ComputersCanvas from './Computer';
 import './HeaderTwo.scss';
 
 const HeaderTwo = () => {
@@ -18,14 +19,16 @@ const HeaderTwo = () => {
                 </h3>
 
             </motion.div>
-            <motion.div
+
+            <ComputersCanvas />
+            {/* <motion.div
                 whileInView={{ x: [100, 0], opacity: [0, 1] }}
                 whileHover={{ scale: 1.1 }}
                 transition={{ duration: 0.75 }}
                 className="header_box second_header_box"
             >
                 <img src={images.about01} alt="header-img" />
-            </motion.div>
+            </motion.div> */}
         </div>
     )
 }
